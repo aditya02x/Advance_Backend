e05cfabf971f17d745cf45d5f989b72f50e2562b
Aditya Raj <adityaraj34@icloud.com>
2026-05-14 22:17:40 +0530

    server.js

--- Day_1/server.js
+++ Day_1/server.js
@@ -1,4 +1,4 @@
-import express from "express";
+
 import dotenv from "dotenv";
 dotenv.config();
 
@@ -8,5 +8,20 @@ import connectDB from "./config/db.js";
 
 const PORT = process.env.PORT || 5000;
 
-connectDB();
+const startServer = async ()=>{
+    try 
+    {
+        await connectDB();
+        app.listen(PORT,()=>{
+            console.log(`Server is running on port ${PORT}`);
+        })
+        
+    } catch (error) {
+        console.log("Error starting server:", error);
 
+        
+    }
+}
+
+
+startServer();
