4bd002c84189626029811d45ff6dc7b15ba6a7ad
Aditya Raj <adityaraj34@icloud.com>
2026-05-14 22:10:17 +0530

    getallproduct

--- Day_1/src/controllers/product.controllers.js
+++ Day_1/src/controllers/product.controllers.js
@@ -26,3 +26,20 @@ export const craeteProduct = async (req,res)=>{
         
     }
 }
+
+
+export const getAllProducts = async (req,res)=>{
+    try {
+        const products = await Product.find();
+        res.status(200).json({
+            message:"Products fetched successfully",
+            data:products
+        })
+
+        
+        
+    } catch (error) {
+        res.status(500).json({message:"Server Error"})
+        console.log(error);
+    }
+}
