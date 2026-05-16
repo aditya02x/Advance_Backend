36dfdc95f8f7695e683be52f811545e8fe19a10d
Aditya Raj <adityaraj34@icloud.com>
2026-05-17 00:14:28 +0530

    rate limite route

--- Day_1/src/middleware/ratelimiter.js
+++ Day_1/src/middleware/ratelimiter.js
@@ -19,3 +19,5 @@ const ratelimiter = asynchandeler(
         }
     }
 )
+
+export default ratelimiter;
--- Day_1/src/routes/product.route.js
+++ Day_1/src/routes/product.route.js
@@ -1,10 +1,11 @@
 import express from "express";
 import { createProduct ,getAllProducts } from "../controllers/product.controllers.js";
+import ratelimiter from "../middleware/ratelimiter.js";
 
 const router = express.Router();
 
 
 router.post("/add",createProduct)
-router.get("/",getAllProducts)
+router.get("/", ratelimiter, getAllProducts)
 
 export default router;
