0853c8deba87a2b493a9489a69be27a9f476147b
Aditya Raj <adityaraj34@icloud.com>
2026-05-18 21:28:08 +0530

    worker bullmq

--- Day_1/src/workers/product.worker.js
+++ Day_1/src/workers/product.worker.js
@@ -1,5 +1,19 @@
 import { Worker } from "bullmq";
 
-const productWorker = new Worker("newproduction")
+const productWorker = new Worker("newproduction" ,
+     async (job)=>{
+         console.log("Job received" , job.name);
+        console.log("Processing job" , job.data);
+
+        console.log(`processing product :`$(job.data.title));
+     } ,
+     {
+        connection:{
+            host:"localhost",
+            port:6379
+        }
+     }
+
+)
 
 export default productWorker;
